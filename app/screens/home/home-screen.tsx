02fe7e1c469366c0967c4e57037f33b0c9022be0
Hiếu Trần <hieu.tran1@mservice.com.vn>
2022-08-21 17:02:35 +0700

    feat: add home screen

---
+++ app/screens/home/home-screen.tsx
@@ -0,0 +1,110 @@
+import React, { FC, useEffect } from "react"
+import { observer } from "mobx-react-lite"
+import { FlatList, ImageStyle, TextStyle, View, ViewStyle, Dimensions } from "react-native"
+import { StackScreenProps } from "@react-navigation/stack"
+import { NavigatorParamList } from "../../navigators"
+import { AutoImage, Header, Screen, Text } from "../../components"
+import { color, spacing } from "../../theme"
+// import { useNavigation } from "@react-navigation/native"
+import { useStores } from "../../models"
+
+const BACKGROUND_IMAGE =
+  "https://i.vimeocdn.com/video/1486234694-2789256b3e16a01f78abaebcc3eeb1ef872b63bc8bfda834c8c4cc6b11fb578b-d"
+
+const WIDTH_DEVICE = Dimensions.get("window").width
+
+const WIDTH_CATEGORY = (WIDTH_DEVICE - spacing[2] * 3) / 2
+
+const ROOT: ViewStyle = {
+  flex: 1,
+  backgroundColor: color.background,
+}
+
+const HEADER: TextStyle = {
+  paddingTop: spacing[3],
+  borderBottomWidth: 1,
+  borderColor: color.palette.lighterGrey,
+}
+const HEADER_TITLE: TextStyle = {
+  fontSize: 18,
+  fontWeight: "bold",
+  textAlign: "center",
+  color: color.palette.black,
+}
+
+const FLAT_LIST: ViewStyle = {
+  paddingVertical: spacing[2],
+}
+
+const CATEGORY_BACKGROUND: ImageStyle = {
+  width: WIDTH_CATEGORY,
+  height: WIDTH_CATEGORY,
+}
+
+const CATEGORY_CONTAINER: ViewStyle = {
+  marginLeft: spacing[2],
+}
+
+const CATEGORY_INFO: ViewStyle = {
+  ...CATEGORY_BACKGROUND,
+  position: "absolute",
+  backgroundColor: color.palette.blackTransparent,
+  justifyContent: "center",
+  alignItems: "center",
+}
+
+const CATEGORY_ICON: ImageStyle = {
+  width: 24,
+  height: 24,
+  tintColor: color.palette.white,
+}
+
+const CATEGORY_NAME: TextStyle = {
+  color: color.palette.white,
+  fontSize: 14,
+  fontWeight: "bold",
+  marginTop: spacing[2],
+}
+
+const CATEGORY_SEPARATOR: ViewStyle = {
+  height: spacing[2],
+}
+
+export const HomeScreen: FC<StackScreenProps<NavigatorParamList, "home">> = observer(
+  function HomeScreen() {
+    const { categoryStore } = useStores()
+    const { categories } = categoryStore
+
+    useEffect(() => {
+      async function fetchData() {
+        await categoryStore.getCategories()
+      }
+
+      fetchData()
+    }, [])
+
+    // const navigation = useNavigation()
+
+    return (
+      <Screen style={ROOT} preset="fixed">
+        <Header headerTx="home.title" style={HEADER} titleStyle={HEADER_TITLE} />
+        <FlatList
+          data={categories}
+          numColumns={2}
+          keyExtractor={(item) => item.key}
+          contentContainerStyle={FLAT_LIST}
+          renderItem={({ item }) => (
+            <View style={CATEGORY_CONTAINER}>
+              <AutoImage source={{ uri: BACKGROUND_IMAGE }} style={CATEGORY_BACKGROUND} />
+              <View style={CATEGORY_INFO}>
+                <AutoImage source={{ uri: item.image }} style={CATEGORY_ICON} />
+                <Text style={CATEGORY_NAME}>{item.name}</Text>
+              </View>
+            </View>
+          )}
+          ItemSeparatorComponent={() => <View style={CATEGORY_SEPARATOR} />}
+        />
+      </Screen>
+    )
+  },
+)
